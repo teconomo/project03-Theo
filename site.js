37e3b3883086eae23f1196daf7faccd91725de5d
Bobby Ton <bobbyt1997@gmail.com>
2016-12-07 16:28:47 -0600

    Changed form name

--- site.js
+++ site.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-   $('#translate-form').on('submit', function(event) {
+   $('#dictionary-form').on('submit', function(event) {
     var query = $('#string').val();
     $.get(
 'https://dictionary.yandex.net/api/v1/dicservice.json/lookup?key=dict.1.1.20161207T050539Z.4477287c82f93220.0ffafbf07a34f70b5034bdfee30d4d7d1ae5f35f&lang=en-en&text=' + query,
