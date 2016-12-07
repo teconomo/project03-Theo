908ef69b35d4f199e5f5347e28ff637c1b3646d9
Ken Ellorando <Ken Ellorando>
2016-12-07 16:12:00 -0600

    More testing of js forms

--- site.js
+++ site.js
@@ -1,17 +1,20 @@
 $(document).ready(function() {
-  
-  $('#translate-form').on('submit', function(event) {
-    alert('Submit Successful!');
-
-    var text = document.getElementById("text").innerHTML;
-    google.language.translate(text, 'es', 'en', function(result) {
-        var translated = document.getElementById("translation");
-        if (result.translation) {
-            translated.innerHTML = result.translation;
-        }
-    });
-
+/* ON SUBMIT OF dictionary FORM WILL EXECUTE */
+   $('#dictionaryform').on('submit', function(event) {
+     
+     /* DONT KNOW WHAT THIS IS */
+    var word = $('#word').val();
+     
+     /* GETS DATA FROM API HERE AND RETURNS */
+    $.get(
+      'https://translate.yandex.net/api/v1.5/tr.json/translate?key={dict.1.1.20161207T070443Z.34b65ed076d1cb0b.c5d3b39d789b9abaafcd74112159922fc3331d6d}&text={#word}&lang={es} 
+      
+      /*https://dictionary.yandex.net/api/v1/dicservice.json/lookup?key=dict.1.1.20161207T070443Z.34b65ed076d1cb0b.c5d3b39d789b9abaafcd74112159922fc3331d6d&lang=en-en&text=#word',*/
+      
+      function(data) {
+        var data = data[0];
+        $('#word').html('Word: ' + word);
+        $('#definition').html('Definition: ' +data.def);
+        });
     event.preventDefault();
-  });
-
 });
