90462a674cacc57b7d60f1a9e875eff075687d28
Bobby Ton <bobbyt1997@gmail.com>
2016-12-07 20:58:29 -0600

    Modified variable names to match html

--- site.js
+++ site.js
@@ -1,13 +1,13 @@
 $(document).ready(function() {
-   $('#dictionary-form').on('submit', function(event) {
-    var search = $("#label").text();
+   $('#summoner-form').on('submit', function(event) {
+    var query = $('#sumLabel').val();
     $.get(
-'https://dictionary.yandex.net/api/v1/dicservice.json/lookup?key=dict.1.1.20161207T050539Z.4477287c82f93220.0ffafbf07a34f70b5034bdfee30d4d7d1ae5f35f&lang=en-en&text=' + search,
+'https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/" + search + "?api_key=RGAPI-1cba763d-5c41-4c45-99cf-f1c75efbed57' + query,
       function(data) {
-        $("#word").html('Word: ' + search);
-        $("#definition").html('Definition: ');
-      });
-  event.preventDefault();
+        var info = data[0];
+        $('#summoner-name').html('Summoner: ' + query);
+        $('#summoner-level').html('Level: ' + info);
+      }); event.preventDefault();
 
 });
 
@@ -16,6 +16,8 @@ $(document).ready(function() {
 /* BACKUP PLAN */
 /* https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/SUMMONERNAME?api_key=RGAPI-1cba763d-5c41-4c45-99cf-f1c75efbed57 */
 
+/*
 $(document).ready(function() {
   
 });
+*/
