34cb0476dfdb9fc580a8dae58076c0aa4c5b1034
Ken Ellorando <Ken Ellorando>
2016-12-07 20:41:25 -0600

    ADDED handles errors in if statement

--- site.js
+++ site.js
@@ -1,21 +1,33 @@
 var sumName = "";
 
 function findSummonerLevel() {
-    var SUMMONER_NAME = "";
-    SUMMONER_NAME = $("#userName").val();
-  
-    /* if input was not empty */
-    if (SUMMONER_NAME !== "") {
-      $.ajax({
-            url: 'https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/' + SUMMONER_NAME + '?api_key=RGAPI-1cba763d-5c41-4c45-99cf-f1c75efbed57',
-            type: 'GET',
-            dataType: 'json',
-            data: {
+  var INPUT = "";
+  INPUT = $("#userName").val();
 
-            },
-            success: function (json) {
-                var SUMMONER_NAME_NOSPACES = SUMMONER_NAME.replace(" ", "");
-    }
-        
-        
+  /* if input was not empty */
+  if (INPUT !== "") {
+    /* Request for data */
+    $.ajax({
+        url: 'https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/' + INPUT + '?api_key=RGAPI-1cba763d-5c41-4c45-99cf-f1c75efbed57',
+        type: 'GET',
+        dataType: 'json',
+        data: {
+
+        },
+
+        /* For names with spaces, necessary to replace */
+        success: function (json) {
+          var REVISEDINPUT = INPUT.replace(" ", "");
+          REVISEDINPUT.toLowerCase().trim();
+          summonerLevel = json[REVISEDINPUT].summonerLevel;
+          document.getElementById("sLevel").innerHTML = summonerLevel;
+          sumName = json[REVISEDINPUT].name;
+        },
+      
+        /* for accounts not found */
+        error: function () {
+          alert("Account does not exist or is banned");
+        }
+        });
+    } else {}
 }
