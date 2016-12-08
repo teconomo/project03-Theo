6611159086f12bce5a8b12ae8207cc6652c5e95d
Ken Ellorando <Ken Ellorando>
2016-12-07 20:57:29 -0600

    CHANGED output for reading the level

--- site.js
+++ site.js
@@ -8,26 +8,26 @@ function findSummonerLevel() {
   if (INPUT !== "") {
     /* Request for data */
     $.ajax({
-        url: 'https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/' + INPUT + '?api_key=RGAPI-1cba763d-5c41-4c45-99cf-f1c75efbed57',
-        type: 'GET',
-        dataType: 'json',
-        data: {
+      url: 'https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/' + INPUT + '?api_key=RGAPI-1cba763d-5c41-4c45-99cf-f1c75efbed57',
+      type: 'GET',
+      dataType: 'json',
+      data: {
 
-        },
+      },
 
-        /* For names with spaces, necessary to replace */
-        success: function (json) {
-          var REVISEDINPUT = INPUT.replace(" ", "");
-          REVISEDINPUT.toLowerCase().trim();
-          summonerLevel = json[REVISEDINPUT].summonerLevel;
-          document.getElementById("sLevel").innerHTML = summonerLevel;
-          sumName = json[REVISEDINPUT].name;
-        },
-      
-        /* for accounts not found */
-        error: function () {
-          alert("Account does not exist or is banned");
-        }
-        });
-    } else {}
+      /* For names with spaces, necessary to replace */
+      success: function (json) {
+        var REVISEDINPUT = INPUT.replace(" ", "");
+        REVISEDINPUT.toLowerCase().trim();
+        summonerLevel = json[REVISEDINPUT].summonerLevel;
+        document.getElementById("sLevel").innerHTML = "Summoner Level: " + summonerLevel;
+        sumName = json[REVISEDINPUT].name;
+      },
+
+      /* for accounts not found */
+      error: function () {
+        alert("Account does not exist or is banned");
+      }
+    });
+  } else {/*nothing*/}
 }
